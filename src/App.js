0313da3b645b3abf38664b130a715cee0e888aab
Chriswenzy <chrischukwuebuka85@gmail.com>
2020-12-25 11:52:07 +0100

    More

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Col, Container, Navbar, Nav, ButtonGroup, Button, Row} from 'react-bootstrap';
+import { Col, Container, Navbar, Nav, ButtonGroup, Button, Row, Card} from 'react-bootstrap';
 
 
 function App() {
   return (
-    <div className="Container" >
+    <div className="wrapper" >
        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Navbar.Brand href="#home">Logo</Navbar.Brand>
         <ButtonGroup aria-label="Basic example" size="sm" className="mr-2">
@@ -15,36 +15,35 @@ function App() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
 
-          <Nav className="mr-auto">
-          <ButtonGroup aria-label="Basic example" size="sm" className="mr-2">
-            <Button variant="dark">Show All</Button>
-            <Button variant="dark">Crypto</Button>
-            <Button variant="dark">Commodities</Button>
-            <Button variant="dark">Stock</Button>
-            <Button variant="dark">Index</Button>
-            <Button variant="dark">Currency</Button>
-          </ButtonGroup>
-          </Nav>
+          <div className="mr-auto justify-content-center">
+            <ButtonGroup aria-label="Basic example" size="sm" className="mr-2">
+              <Button variant="dark">Show All</Button>
+              <Button variant="dark">Crypto</Button>
+              <Button variant="dark">Commodities</Button>
+              <Button variant="dark">Stock</Button>
+              <Button variant="dark">Index</Button>
+              <Button variant="dark">Currency</Button>
+            </ButtonGroup>
+          </div>
 
           <Nav>
             <Nav.Link href="#deets"> 
             <i className="fa fa-search"></i>
             </Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
-              Dank memes
+            <img src="http://placehold.it/150x150" className="navbar-img" alt="Profile Image" />
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
 
-      <Container fluid>
-        <Row>
-          <Col md={4}> 
-          <h6 className="text-color">OPEN TRACKS</h6>
-          </Col>
-          <Col md={8}> content</Col>
-      </Row>
-      </Container>
+      <div className="sidebar">
+      <h6 className="text-color">OPEN TRACKS</h6>
+
+      </div>
+      <div className="content">
+        <h4>content</h4>
+      </div>
 
     </div>
     
